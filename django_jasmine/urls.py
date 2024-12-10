6bf8fb6b65b15b56aa585ce6abb4f7b551b57013
Pekka Lampila <pekka.lampila@hypeinnovation.com>
2024-12-10 15:18:18 +0200

    Replace url with re_path for django 4.x compatibility

--- django_jasmine/urls.py
+++ django_jasmine/urls.py
@@ -1,6 +1,6 @@
 import os
 
-from django.conf.urls import url
+from django.urls import re_path
 from django.conf import settings
 from django.views.static import serve
 
@@ -9,14 +9,14 @@ from .views import DjangoJasmineView
 dj_jas_view = DjangoJasmineView.as_view()
 
 urlpatterns = [
-    url(r'^tests/(?P<path>.*)$', serve, {
+    re_path(r'^tests/(?P<path>.*)$', serve, {
         'document_root': os.path.join(settings.JASMINE_TEST_DIRECTORY, "spec"),
     }, name='jasmine_test'),
-    url(r'^fixtures/(?P<path>.*)$', serve, {
+    re_path(r'^fixtures/(?P<path>.*)$', serve, {
         'document_root': os.path.join(
             settings.JASMINE_TEST_DIRECTORY, "fixtures",
         ),
     }, name='jasmine_fixtures'),
-    url('^$', dj_jas_view, name='jasmine_default'),
-    url('^(?P<version>.*)/$', dj_jas_view, name='jasmine_version'),
+    re_path('^$', dj_jas_view, name='jasmine_default'),
+    re_path('^(?P<version>.*)/$', dj_jas_view, name='jasmine_version'),
 ]
